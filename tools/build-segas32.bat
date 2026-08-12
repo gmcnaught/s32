de37e22a105eae51ebbbdb6f52e33265e33280da
meathax <meathax@gmail.com>
2026-08-12 19:40:01 +1000

    Harden production build configuration

--- tools/build-segas32.bat
+++ tools/build-segas32.bat
@@ -3,5 +3,5 @@ REM Thin wrapper: build the segas32 profile (Sonic and future non-V25
 REM games, HLE only). See tools/build.bat for the actual pipeline.
 set S32_PROJECT=segas32
 set S32_REVISION=segas32
-set S32_RELEASE_NAME=segas32
+if not defined S32_RELEASE_NAME set S32_RELEASE_NAME=segas32
 call "%~dp0build.bat" %*
